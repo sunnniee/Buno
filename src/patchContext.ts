67a0c6992d6492db1cc9a22b36aa3e15a8467528
ActuallyTheSun <78964224+ActuallyTheSun@users.noreply.github.com>
2023-03-05 12:18:18 +0200

    error handling

---
+++ src/patchContext.ts
@@ -0,0 +1,17 @@
+import { AnyInteractionGateway } from "oceanic.js"
+import { onMsgError } from "./constants.js"
+
+export function patch<T = AnyInteractionGateway>(ctx: T): T {
+    const methods = [
+        "createFollowup", "createMessage", "createModal",
+        "deleteFollowup", "deleteOriginal",
+        "editOriginal", "editFollowup", "editParent",
+        "getFollowup", "getOriginal",
+    ] as const
+    methods.forEach(m => ctx[m] && (ctx[m] = new Proxy(ctx[m], {
+        apply(target, thisArg, argArray) {
+            return target.apply(thisArg, argArray).catch(e => onMsgError(e, ctx as any))
+        },
+    })))
+    return ctx
+}
